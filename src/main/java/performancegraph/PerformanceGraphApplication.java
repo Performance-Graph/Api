4f537d11866c0b87f892789b46d24089c5e7c54e
junghyunlyoo <junghyunlyooinjobis@jobis.co>
2021-10-16 02:25:55 +0900

    init application java file

---
+++ src/main/java/performancegraph/PerformanceGraphApplication.java
@@ -0,0 +1,13 @@
+package performancegraph;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class PerformanceGraphApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PerformanceGraphApplication.class, args);
+    }
+}
