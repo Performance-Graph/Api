e6de9fbf0917e94f9824164c435873d7c14622fc
박준호 <zkdlwnfm1@oozooinc.com>
2021-10-25 22:40:25 +0900

    Add lombok, Update Sports Entity

--- src/main/java/performancegraph/core/sports/domain/Sports.java
+++ src/main/java/performancegraph/core/sports/domain/Sports.java
@@ -1,12 +1,31 @@
 package performancegraph.core.sports.domain;
 
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "sports")
 public class Sports {
     @Id
     private Long sportsId;
+
+    @Column(nullable = false)
+    private String position;
+    @Column(nullable = false)
+    private String nationality;
+    @Column(nullable = false)
+    private int height;
+    @Column(nullable = false)
+    private int weight;
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime createAt;
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updateAt;
 }
