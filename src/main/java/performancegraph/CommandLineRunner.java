050cfc92914fb64935583fcb3f52b502c10858f0
junghyunlyoo <junghyunlyooinjobis@jobis.co>
2021-10-29 00:22:10 +0900

    init config

---
+++ src/main/java/performancegraph/CommandLineRunner.java
@@ -0,0 +1,7 @@
+package performancegraph;
+
+public class CommandLineRunner implements org.springframework.boot.CommandLineRunner {
+    @Override
+    public void run(String... args) {
+    }
+}
--- src/main/java/performancegraph/PerformanceGraphApplication.java
+++ src/main/java/performancegraph/PerformanceGraphApplication.java
@@ -1,11 +1,10 @@
 package performancegraph;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PerformanceGraphApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PerformanceGraphApplication.class, args);
     }
